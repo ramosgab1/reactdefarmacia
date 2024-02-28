4acc17ed3cb5d616223a1563c434b782ad2b7011
ramosgab1 <gabrieller_david@hotmail.com>
2024-02-28 12:26:28 -0300

    CRUD-CATEGORIAS, ROTAS + Ajuste na Service P/ persistir Dados de EDIÇÃO.

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -4,21 +4,21 @@ const api = axios.create({
   baseURL: 'https://farmaciaapi-0496.onrender.com'
 })
 
-export const buscar = async(url: string, setDados: Function, header: Object) => {
-  const resposta = await api.get(url, header)
+export const buscar = async(url: string, setDados: Function) => {
+  const resposta = await api.get(url)
   setDados(resposta.data)
 }
 
-export const cadastrar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-  const resposta = await api.post(url, dados, header)
+export const cadastrar = async(url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.post(url, dados)
   setDados(resposta.data)
 }
 
-export const atualizar = async(url: string, dados: Object, setDados: Function, header: Object) => {
-  const resposta = await api.put(url, header)
+export const atualizar = async(url: string, dados: Object, setDados: Function) => {
+  const resposta = await api.put(url, dados)
   setDados(resposta.data)
 }
 
-export const deletar= async(url: string, header: Object) => {
-  await api.delete(url, header)
+export const deletar= async(url: string) => {
+  await api.delete(url)
 }
